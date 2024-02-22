254395eed279681fa7281cf05219b7e7c8c46de4
Davi Morais <davifarias2433@gmail.com>
2024-02-21 22:34:09 -0300

    Gerando palavras aleatorias
    Implementacao da primeira parte do algoritmo, geracao de palavras aleatorias.

--- file_generator.py
+++ file_generator.py
@@ -1,3 +1,5 @@
+from random import randint
+
 def file_generator(n, alfabeto, minimo, maximo, split):
     """Gera n palavras aleatorias que podem possuir qualquer tamanho
     entre um minimo e um maximo. As palavras geradas devem ser divididas(split)
@@ -10,3 +12,21 @@ def file_generator(n, alfabeto, minimo, maximo, split):
         maximo (int): tamanho maximo que uma palavra pode ter
         split (int): quantidade de arquivos que devem ser gerados
     """
+
+
+    palavras_geradas = []
+
+
+    for _ in range(n):
+        numero_letras = randint(minimo, maximo)
+        
+        nova_palavra = "".join([alfabeto[randint(0, len(alfabeto)-1)] for _ in range(numero_letras)])
+
+        palavras_geradas.append(nova_palavra)
+    
+
+    return palavras_geradas
+
+
+# palavras = file_generator(20, "abcdef", 2, 6, 10)
+# print(palavras)
