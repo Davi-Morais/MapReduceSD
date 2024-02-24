92dcbb2950d6c0128daeb23dc20655358ce96dce
Davi Morais <davifarias2433@gmail.com>
2024-02-24 15:30:36 -0300

    Primeira implementacao de EmitirIntermediario

--- Map.py
+++ Map.py
@@ -1,4 +1,22 @@
 from pathlib import Path
+from os import path
+
+def EmitirIntermediario(chave, ocorrencia):
+    """Escreve em um arquivo intermediario um valor chave 
+    seguido de sua acorrencia
+
+    Args:
+        chave (string): Nome da chave
+        ocorrencia (char): o total de ocorrencias
+    """
+
+    intermediario = "./intermediario"
+    if not path.exists(intermediario):
+        open(intermediario, 'w').close()
+
+    with open(intermediario, "a") as arquivo:
+        arquivo.write("{} {}\n".format(chave, ocorrencia))
+
 
 def Map(path_arquivo):
     """Lê um arquivo e emite a ocorrência de uma mesma palavra
@@ -11,5 +29,5 @@ def Map(path_arquivo):
     palavras = arquivo.read_text(encoding="utf-8").split()
     
     for palavra in palavras:
-        # Emitir intermediario
+        EmitirIntermediario(palavra, '1')
         pass
